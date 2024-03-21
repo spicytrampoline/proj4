46450957a9b455f0f71a21475c54ad572ed0cb6b
Savannah Bosley <103013493+spicytrampoline@users.noreply.github.com>
2024-03-20 21:52:00 -0700

    Update TinyFS_errno.h

---
+++ 453-TinyFS-Disk-Emulator/TinyFS_errno.h
@@ -0,0 +1,14 @@
+#define OPEN_DISK           -1
+#define INVALID_NBYTES      -2
+#define FREE_BLOCKS_MAXXED  -3
+#define TABLE_FULL          -4
+#define WRITE_BLOCK         -5
+#define READ_BLOCK          -6
+#define MAGIC_NUM           -7
+#define MOUNT               -8
+#define CLOSE_DISK          -9
+#define INVALID_NAME        -10
+#define INVALID_FD          -11
+#define FREE_BLOCK_MAXXED   -12
+#define EOFF                -13
+#define NONEXISTANT_FILE    -14
